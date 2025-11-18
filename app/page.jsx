b391c75693c7c9e9af02eafd5e79696df2b082bb
thekevinkun <developer.kevinkun@gmail.com>
2025-11-18 19:34:20 +0800

    Change layout and page file

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "@/components/Sidebar";
+import MainContent from "@/components/MainContent";
+
+export default function Home() {
+  return (
+    <main className="cv-layout">
+      <Sidebar />
+      <MainContent />
+    </main>
+  );
+}
